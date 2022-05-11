57e5f5a05a1446c7a6348e1b88c8745280e771d0
Mud <skymudz@gmail.com>
2022-05-11 16:18:02 +0200

    ShopItem can now be used. It has to be given a Plant Object which can then be used to set the texture and cost of the ShopItem. A signal has been implemented to be emitted when the button has been clicked. This signal can be used by a shop, which can add a ShopItem, to determine when a plant has been requested. The signal also includes a plant object to tell the Shop which plant has to be instantiated.

---
+++ UI/ShopItem.gd
@@ -0,0 +1,35 @@
+extends MarginContainer
+# Handles the ShopItem along with its UI
+
+
+# SIGNALS
+signal buy_plant(plant)
+
+# VARIABLES
+var plant: Plant
+var plantInfo: MarginContainer
+
+# ENGINE METHODS
+
+# Displays the given data on the UI using the plant variable
+# and connects a signal for changing the balance on the UI
+func _ready() -> void:
+	$VBoxContainer/TextureRect.texture = plant.texture
+	$VBoxContainer/Label.text = str(plant.cost)
+
+# UI updates
+
+# Toggle the visibility of the tooltip on the UI
+func _showTooltip() -> void:
+	$Frame/PlantInfo.setValues(plant)
+	$Frame/PlantInfo.show()
+	$TooltipTimer.stop()
+
+
+# SIGNAL METHODS
+
+# Signals that a plant is about to be bought to the Shop
+# Connects a signals to recognize if the plant has been placed
+func _on_Button_button_down() -> void:
+	emit_signal("buy_plant", plant)
+	Global.main.get_node("PlacementSystem").connect("plant_placed", self, "_on_plant_placed", [], CONNECT_ONESHOT)
