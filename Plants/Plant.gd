352f20f13fad5bfb3125d0a65ae61ef45d881622
Mud <skymudz@gmail.com>
2022-05-11 16:12:22 +0200

    Added Plant Class to define various parameters for a plant

---
+++ Plants/Plant.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name Plant
+# Class which specifies stats, sprites and scripts for plants
+
+
+export(String) var name
+export(int) var hp
+export(int) var cost
+export(float) var cooldown
+export(float) var attackInterval
+export(float) var attackRange
+export(float) var animationSpeedScale = 1.0
+export(SpriteFrames) var spriteFrames0
+export(SpriteFrames) var spriteFrames1
+export(SpriteFrames) var spriteFrames2
+export(Texture) var texture
+export(Script) var plantScript
