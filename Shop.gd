827e6228aba15e0f338247b5b050446682b3b206
Mud <skymudz@gmail.com>
2022-05-12 08:04:28 +0200

    Added Shop which automatically adds plants that are found in the project files to itself. For this to work, a PlantBlueprint is needed which is not yet ready

--- Shop.gd
+++ Shop.gd
@@ -1,5 +1,24 @@
-extends MarginContainer
+extends VBoxContainer
+# handles the Shop along with its UI
 
 
-func addItem(plant: Plant) -> void:
-	
+var ShopItem: PackedScene = preload("res://UI/ShopItem.tscn")
+
+# SIGNALS
+signal plant_selected(plant)
+
+# ENGINE METHODS
+
+# Creates the Shopitems and connects the needed signals
+func _ready() -> void:
+	for x in Directories.getEntities(Directories.dirPlants):
+		var shopItem: MarginContainer = ShopItem.instance()
+		shopItem.plant = load(x)
+		shopItem.connect("buy_plant", self, "_on_buy_plant")
+		add_child(shopItem)
+
+# SIGNAL METHODS
+
+# Signals the plant select to the Main Node
+func _on_buy_plant(plant: Plant) -> void:
+	emit_signal("plant_selected", plant)
