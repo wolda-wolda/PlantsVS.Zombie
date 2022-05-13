b687c94f665a7bd51902d3a0004f0b3a749f9738
Mud <skymudz@gmail.com>
2022-05-13 12:05:08 +0200

    Added the ability for a sunflower to spawn suns, sun scene still needs to be created

--- Plants/Sunflower/Sunflower.gd
+++ Plants/Sunflower/Sunflower.gd
@@ -1,2 +1,10 @@
 extends "res://Plants/GeneralPlant.gd"
 
+
+var Sun: PackedScene = null
+
+# Spawn a sun
+func performAction() -> void:
+	var sun: Node2D = Sun.instance()
+	add_child(sun)
+	sun.get_node("AnimationPlayer").play("Spawn")
