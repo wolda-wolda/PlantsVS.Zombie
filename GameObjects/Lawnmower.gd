f82e83a7cb1bbb11fecc9a925f03c70648a87cef
Mud <skymudz@gmail.com>
2022-05-19 08:25:19 +0200

    Reworked Lawnmower

--- GameObjects/Lawnmower.gd
+++ GameObjects/Lawnmower.gd
@@ -1,14 +1,30 @@
 extends Node2D
+# Lawnmower which activates when Zombies reach the far left of the map
+# Kills all Zombies on its way to the right and disappears after that
 
-var movementSpeed:float = 100.0
+# VARIABLES
+var distance_travelled: float = 0.0
 
-func _process(delta: float) -> void:
-	position.x += movementSpeed * delta
-	
+# ENGINE METHODS
+
+# Deactivates the _process method
 func _ready() -> void:
 	set_process(false)
 
+# Makes the lawnmower move to the right
+# Frees itself when it travels outside of the playing area
+func _process(delta: float) -> void:
+	var speed: float = delta * 100
+	position += Vector2(speed, 0)
+	distance_travelled += speed
+	if distance_travelled > 250:
+		queue_free()
 
-func _on_HitHurtbox_area_entered(area: Area2D) -> void:
+# SIGNAL METHODS
+
+# When a Zombie enters the area, start moving and start shaking animation
+# Damage Zombies upon entering the area
+func _on_Area2D_area_entered(area: Area2D) -> void:
 	set_process(true)
-	$AnimationPlayer.start("Shake")
+	area.get_parent().takeDamage(1000)
+	$AnimationPlayer.play("Shake")
