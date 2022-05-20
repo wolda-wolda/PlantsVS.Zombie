0455c14159910d52e5ec18f8dd0a240ad418b94c
Mud <skymudz@gmail.com>
2022-05-20 11:44:31 +0200

    Converted WinAnimation to text animation and generalized the behavior. A text can be passed as an argument and will then be animated to scale up from the center of the screen

--- Effects/TextAnimation.gd
+++ Effects/TextAnimation.gd
@@ -1,7 +1,16 @@
 extends CenterContainer
 
 
+export(String) var text
+
 func _ready() -> void:
+	$Label.text = text
+
+func _on_Label_resized() -> void:
+	$Label.rect_pivot_offset = $Label.rect_size / 2
+	startAnimation()
+
+func startAnimation() -> void:
 	var tween: = Tween.new()
 	tween.interpolate_property($Label, "rect_scale", Vector2(1,1), Vector2(5,5), 1, Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
 	add_child(tween)
