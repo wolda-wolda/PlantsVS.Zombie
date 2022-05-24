e2494518883bbdce728183a142bf450d1ba59fc0
Marcel <marcelwalder12@gmail.com>
2022-05-24 11:02:03 +0200

    Animated progress bar and added documentation
    Signed-off-by: Marcel <marcelwalder12@gmail.com>

--- UI/ProgressBar.gd
+++ UI/ProgressBar.gd
@@ -1,18 +1,33 @@
 extends TextureProgress
 # Progress bad to display the match progress
 
+
+# VARIABLES
+
+var localValue: int = 0
+var tween: Tween
+
 #GETTERS
 
 # Get the progress value
-func getValue() -> float:
-	return value
+func getValue() -> int:
+	return localValue
 
 # SETTERS
 
 # Set the progress value
 func setValue(newValue : int) -> void:
-	value = newValue
+	localValue = newValue
+	tween = Tween.new()
+	tween.interpolate_property(self, "value", value, localValue, 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	add_child(tween)
+	tween.start()
 
 # Set the maximum value of the progress bar
 func setMax(newMax: int)  -> void:
 	max_value = newMax
+
+# SIGNAL METHODS
+
+func _onTweenCompleted() -> void:
+	tween.queue_free()
