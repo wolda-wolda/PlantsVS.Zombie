51025cf2285fb526a357443d49f60bf606d95343
simk12 <simrankaran99@gmail.com>
2022-05-31 10:26:35 +0200

    MainMenu designed and added logic to buttons

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -1,2 +1,16 @@
 extends CenterContainer
 
+
+
+func _on_Play_pressed():
+	$VBoxContainer/Play.visible = false
+	$VBoxContainer/MultiPlayer.visible = true
+	$VBoxContainer/SinglePlayer.visible = true
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+
+func _on_SinglePlayer_pressed():
+	pass # Replace with function body.
