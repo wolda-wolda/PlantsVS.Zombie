264ce18371ea0002c722d7541decea07683b21b6
Mud <skymudz@gmail.com>
2022-05-31 10:42:55 +0200

    Fixed wrong code

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -12,6 +12,6 @@ func _on_Exit_pressed():
 	get_tree().quit()
 
 func _on_SinglePlayer_pressed():
-	var mainlevel: Node2D = Entities.Mainlevel.intance()
+	var mainlevel: Node2D = Entities.MainLevel.instance()
 	mainlevel.name = "MainLevel"
 	Global.main.add_child(mainlevel)
